e137989faeee07ca554802bb34399372cef942d8
Bas Tolen <bastolen97@gmail.com>
2017-06-06 14:12:15 +0200

    menuitemdao en bestellingdao toegevoegd

---
+++ Chapoo/DAL/BestellingDAO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using System.Data.SqlTypes;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DAL
+{
+    class BestellingDAO
+    {
+        protected SqlConnection dbConnection;
+
+        public BestellingDAO()
+        {
+            string connString = ConfigurationManager.ConnectionStrings["connstring"].ConnectionString;
+            dbConnection = new SqlConnection(connString);
+        }
+
+        public Bestelling ReadBestelling(SqlDataReader reader)
+        {
+            Bestelling b = new Bestelling()
+        }
+    }
+}
---
+++ Chapoo/DAL/MenuitemDAO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using System.Data.SqlTypes;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DAL
+{
+    class MenuitemDAO
+    {
+        protected SqlConnection dbConnection;
+
+        public MenuitemDAO()
+        {
+            string connString = ConfigurationManager.ConnectionStrings["connstring"].ConnectionString;
+            dbConnection = new SqlConnection(connString);
+        }
+
+        public MenuItem ReadItem(SqlDataReader reader)
+        {
+            MenuItem m = new MenuItem();
+            int id = (int)reader["MenuItem_id"];
+            string naam = (string)reader["Naam"];
+            //Categorie
+            //Dagdeel
+            //Is_alcoholisch
+            float Prijs = (float)reader["Prijs"];
+            int Voorraad = (int)reader["Voorraad"];
+            string Omschrijving = (string)reader["Omschrijving"];
+        }
+    }
+}
