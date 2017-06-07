50cad6891cd8c2773ad5471cf3e4beb76a2ec626
Florian van Randen <fvanranden@gmail.com>
2017-06-07 17:21:20 +0200

    BestellingDAO aangepast, begonnen aan bestelscherm form

--- Chapoo/DAL/BestellingDAO.cs
+++ Chapoo/DAL/BestellingDAO.cs
@@ -20,12 +20,75 @@ namespace DAL
             dbConnection = new SqlConnection(connString);
         }
 
-        public Bestelling ReadBestelling(SqlDataReader reader)
+        public Bestelling ReadBestelling(int id)
         {
-            Bestelling b = new Bestelling();
-            int id = (int)reader["Bestelling_id"];
-            int medewerkersId = (int)reader["Bestelling_id"];
-            int Tafeld = (int)reader["Bestelling_id"];
+            string queryString =
+            "SELECT * FROM dbo.Bestelling WHERE Bestelling_id = @id;";
+            
+
+            using (dbConnection)
+            {
+                SqlCommand command = new SqlCommand(queryString, dbConnection);
+                dbConnection.Open();
+
+                SqlParameter BestellingIdParam = new SqlParameter("@id", System.Data.SqlDbType.Int);
+                BestellingIdParam.Value = id;
+                command.Parameters.Add(BestellingIdParam);
+
+                command.Prepare();
+                SqlDataReader reader = command.ExecuteReader();
+
+                // Call Read before accessing data.
+                // de code hieronder heb ik laten staan voor als iemand daar een voorbeeld van wilde. groetjes florian.
+
+                //while (reader.Read())
+                //{
+                //}
+
+                int medewerkersId = (int)reader["Werknemer_id"];
+                int TafelId = (int)reader["Tafel_id"];
+                DateTime tijd = (DateTime)reader["Tijd"];
+                Bestelling b = new Bestelling(id, medewerkersId, TafelId, tijd);
+                // Call Close when done reading.
+                reader.Close();
+                dbConnection.Close();
+                return b;
+            }
+
+        }
+
+        public List<Bestelling> ReadBestellingen()
+        {
+            string queryString =
+            "SELECT * FROM dbo.Bestelling;";
+
+            using (dbConnection)
+            {
+                SqlCommand command = new SqlCommand(queryString, dbConnection);
+                dbConnection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                // Call Read before accessing data.
+
+                List<Bestelling> bestelingen = new List<Bestelling>();
+                Bestelling b;
+                while (reader.Read())
+                {
+                    int id = (int)reader["Bestelling_id"];
+                    int medewerkersId = (int)reader["Werknemer_id"];
+                    int TafelId = (int)reader["Tafel_id"];
+                    DateTime tijd = (DateTime)reader["Tijd"];
+                    b = new Bestelling(id, medewerkersId, TafelId, tijd);
+                    bestelingen.Add(b);
+                }
+
+                
+                // Call Close when done reading.
+                reader.Close();
+                dbConnection.Close();
+                return bestelingen;
+            }
 
         }
     }
---
+++ Chapoo/UI/BestellingOpneemScherm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace UI
+{
+    partial class BestellingOpneemScherm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_VorrigeBestelscherm = new System.Windows.Forms.Button();
+            this.btn_AnnuleerBestelscherm = new System.Windows.Forms.Button();
+            this.btn_VerzendenBestelscherm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_VorrigeBestelscherm
+            // 
+            this.btn_VorrigeBestelscherm.Location = new System.Drawing.Point(-4, 591);
+            this.btn_VorrigeBestelscherm.Name = "btn_VorrigeBestelscherm";
+            this.btn_VorrigeBestelscherm.Size = new System.Drawing.Size(139, 69);
+            this.btn_VorrigeBestelscherm.TabIndex = 0;
+            this.btn_VorrigeBestelscherm.Text = "Vorrige";
+            this.btn_VorrigeBestelscherm.UseVisualStyleBackColor = true;
+            // 
+            // btn_AnnuleerBestelscherm
+            // 
+            this.btn_AnnuleerBestelscherm.Location = new System.Drawing.Point(132, 591);
+            this.btn_AnnuleerBestelscherm.Name = "btn_AnnuleerBestelscherm";
+            this.btn_AnnuleerBestelscherm.Size = new System.Drawing.Size(139, 69);
+            this.btn_AnnuleerBestelscherm.TabIndex = 1;
+            this.btn_AnnuleerBestelscherm.Text = "Annuleren";
+            this.btn_AnnuleerBestelscherm.UseVisualStyleBackColor = true;
+            // 
+            // btn_VerzendenBestelscherm
+            // 
+            this.btn_VerzendenBestelscherm.Location = new System.Drawing.Point(267, 591);
+            this.btn_VerzendenBestelscherm.Name = "btn_VerzendenBestelscherm";
+            this.btn_VerzendenBestelscherm.Size = new System.Drawing.Size(139, 69);
+            this.btn_VerzendenBestelscherm.TabIndex = 2;
+            this.btn_VerzendenBestelscherm.Text = "Verzenden";
+            this.btn_VerzendenBestelscherm.UseVisualStyleBackColor = true;
+            // 
+            // BestellingOpneemScherm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 650);
+            this.Controls.Add(this.btn_VerzendenBestelscherm);
+            this.Controls.Add(this.btn_AnnuleerBestelscherm);
+            this.Controls.Add(this.btn_VorrigeBestelscherm);
+            this.Name = "BestellingOpneemScherm";
+            this.Text = "BestellingOpneemScherm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_VorrigeBestelscherm;
+        private System.Windows.Forms.Button btn_AnnuleerBestelscherm;
+        private System.Windows.Forms.Button btn_VerzendenBestelscherm;
+    }
+}
