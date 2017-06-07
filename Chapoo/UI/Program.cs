89c82aceb01bf428d026ac5b8ad94ebc44113810
Florian van Randen <fvanranden@gmail.com>
2017-06-07 22:18:06 +0200

    alles verneukt

--- Chapoo/UI/Program.cs
+++ Chapoo/UI/Program.cs
@@ -17,7 +17,8 @@ namespace UI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-           // Application.Run(new Form1());                 niet verwijderen. gwn veranderen.
+
+            Application.Run();                // niet verwijderen. gwn veranderen.
         }
     }
 }
