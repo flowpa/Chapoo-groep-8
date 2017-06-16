d65fbb76b0347cd3beba6c4b78e170fc6ce9a0df
Bas Tolen <bastolen97@gmail.com>
2017-06-16 13:17:36 +0200

    Deel Bas Af

--- Chapoo/DAL/MenuitemDAO.cs
+++ Chapoo/DAL/MenuitemDAO.cs
@@ -105,18 +105,18 @@ namespace DAL
 
         public MenuItem GetItemById(int id)
         {
-
             SqlCommand cmd = new SqlCommand("SELECT * FROM MenuItem WHERE Menuitem_id = @menuItem_id ", dbConnection);
             SqlParameter idParam = new SqlParameter("@menuItem_id", System.Data.SqlDbType.Int, 32);
-
+            idParam.Value = id.ToString();
             cmd.Parameters.Add(idParam);
+
             dbConnection.Open();
             cmd.Prepare();
+
             SqlDataReader reader = cmd.ExecuteReader();
 
-            MenuItem m = ReadMenuItem(reader);
-            
-            
+            reader.Read();
+            MenuItem m = ReadMenuItem(reader);      
             reader.Close();
             dbConnection.Close();
 
